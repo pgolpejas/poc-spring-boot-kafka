e3eb17609a27d7464d724a986f1eb589a87a78e7
Pedro Sánchez <pesanchez@hotelbeds.com>
2023-06-23 11:55:11 +0200

    add dockerhub deploy

--- src/main/java/com/pocspringbootkafka/hotelavailability/adapters/api/HotelAvailabilitySearchDto.java
+++ src/main/java/com/pocspringbootkafka/hotelavailability/adapters/api/HotelAvailabilitySearchDto.java
@@ -13,32 +13,22 @@ import org.hibernate.validator.constraints.Range;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @ValidDateRange
 public record HotelAvailabilitySearchDto(
 
-    @NotEmpty
-    @Size(max = 7)
-    @Pattern(regexp = "\\d{4}[A-Za-z]{3}")
-    @Schema(example = "1234Abd")
-    String hotelId,
+    @NotEmpty @Size(max = 7) @Pattern(regexp = "\\d{4}[A-Za-z]{3}") @Schema(example = "1234Abd") String hotelId,
 
-    @NotNull
-    @FutureOrPresent
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    @Schema(type = "string", pattern = "dd/MM/yyyy", description = "date in format dd/MM/yyyy", example = "01/07/2023")
-    LocalDate checkIn,
+    @NotNull @FutureOrPresent @JsonFormat(pattern = "dd/MM/yyyy") @Schema(type = "string", pattern = "dd/MM/yyyy",
+                                                                          description = "date in format dd/MM/yyyy",
+                                                                          example = "01/07/2023") LocalDate checkIn,
 
-    @NotNull
-    @FutureOrPresent
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    @Schema(type = "string", pattern = "dd/MM/yyyy", description = "date in format dd/MM/yyyy", example = "30/07/2023")
-    LocalDate checkOut,
+    @NotNull @FutureOrPresent @JsonFormat(pattern = "dd/MM/yyyy") @Schema(type = "string", pattern = "dd/MM/yyyy",
+                                                                          description = "date in format dd/MM/yyyy",
+                                                                          example = "30/07/2023") LocalDate checkOut,
 
-    @NotEmpty
-    @Size(max=10)
-    @Schema(example = "[1,4,34,35]")
-    List<@Range(min = 0, max = 120) Integer> ages) {
+    @NotEmpty @Size(max = 10) @Schema(example = "[1,4,34,35]") List<@Range(min = 0, max = 120) Integer> ages) {
 
     public HotelAvailabilitySearchDto(@NotEmpty @Size(max = 7) @Pattern(regexp = "\\d{4}[A-Za-z]{3}") @Schema(example = "1234Abd")
     String hotelId,
@@ -50,7 +40,12 @@ public record HotelAvailabilitySearchDto(
         this.hotelId = hotelId;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        this.ages = Collections.unmodifiableList(ages);
+        if (Objects.nonNull(ages)) {
+            this.ages = Collections.unmodifiableList(ages);
+        } else {
+            this.ages = null;
+        }
+
     }
 }
 
